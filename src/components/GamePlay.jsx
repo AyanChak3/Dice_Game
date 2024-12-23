7f3b1d03fe84728a5210d629e230033907a2a246
AyanChak3 <>
2024-12-23 19:06:24 +0530

    Final commit

---
+++ src/components/GamePlay.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import NumberSelector from "./NumberSelector"
+import RoleDice from "./RoleDice";
+import TotalScore from "./TotalScore"
+import styled from "styled-components";
+import {Button, OutlineButton} from "../styled/Button"
+import Rules from "./Rules";
+const GamePlay = () => {
+
+  const [score,setScore] = useState(0);
+  const [selectedNumber, setSelectedNumber] = useState();
+  const [currentDice,setCurrentDice] = useState(1);
+  const [error,setError] = useState("");
+  const [showRules,setshowRules] = useState(false)
+  const generateRandomNumber = (min,max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+
+  const roleDice = () => {
+    if(!selectedNumber){
+      setError("You have not selected any number")
+      return;
+    } 
+    
+    const randomNumber = generateRandomNumber(1,7);
+    setCurrentDice((prev) => randomNumber)
+    
+    
+    if(selectedNumber === randomNumber){
+      setScore((prev)=>prev + randomNumber);
+    }else{
+      setScore((prev)=>prev - randomNumber)
+    }
+
+    setSelectedNumber(undefined);
+  };
+
+  const resetScore = () => {
+    setScore(0);
+  }
+
+
+  return (
+    <MainContainer>
+      <div className="top_section">
+        <TotalScore score={score}/>
+        <NumberSelector 
+        error={error}
+        setError = {setError}
+        selectedNumber={selectedNumber}
+        setSelectedNumber={setSelectedNumber}
+        />
+      </div>
+      <RoleDice 
+      currentDice={currentDice}
+      roleDice={roleDice}
+      />
+      <div>
+        <div className="btns">
+          <OutlineButton onClick={resetScore}>Reset</OutlineButton>
+          <Button
+          onClick ={()=> setshowRules((prev)=>!prev)}
+          >{showRules?"Hide":"Show"}Rules</Button>
+        </div>
+      </div>
+      {showRules && <Rules />}
+    </MainContainer>
+  );
+};
+
+export default GamePlay;
+
+const MainContainer = styled.main`
+padding-top : 70px;
+  .top_section{
+    display : flex;
+    justify-content: space-between;
+    align-items: end;
+  }
+  .btns{
+    margin-top: 40px;
+    display : flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap : 10px;
+  }
+`
